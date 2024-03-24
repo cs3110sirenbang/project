7060bcedfadfe0dcce2ba330d3504b122db02fc1
Steven Yu <ssy38@cornell.edu>
2024-03-23 22:36:24 -0400

    Fixed skeleton of document.mli
    Co-authored-by: Zhongxuan Wang <cndanielwang@gmail.com>
    Co-authored-by: Thomas Lu <ThomasLu2333@users.noreply.github.com>
    Co-authored-by: Jasmine Li
    <jxl9@cornell.edu>

--- lib/document.mli
+++ lib/document.mli
@@ -26,15 +26,3 @@ val document_id : t -> string
 
 val data : t -> (key * value) list
 (** return the data of a document *)
-
-type query =
-  | Is_less_than of value
-  | Is_less_than_or_equal_to of value
-  | Is_equal_to of value
-  | Is_greater_than of value
-  | Is_greater_than_or_equal_to of value
-  | Is_not_equal_to of value
-  | Is_in of value list
-  | Is_not_in of value list
-
-val where_field : t -> key -> query

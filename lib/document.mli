864c4f26f62d51c360e129f5a8619b367d08c38e
Steven Yu <ssy38@cornell.edu>
2024-03-24 00:08:02 -0400

    Clarified document.mli specs
    Co-authored-by: Thomas Lu <ThomasLu2333@users.noreply.github.com>

--- lib/document.mli
+++ lib/document.mli
@@ -1,27 +1,32 @@
 type key = string
-(** Type for the key stored in documents *)
+(** Type for the key stored in documents. *)
 
 type value
-(** Type for the values stored in documents *)
+(** Type for the values stored in documents. *)
 
 type t
-(** The representation of the document. A document is a map-like structure
+(** The representation type of the document. A document is a map-like structure
     storing key-value pairs. *)
 
 val set_data : (key * value) list -> t -> t
-(** overwrite a document (only retain id)*)
+(** [set_data pairs document] is the document containing only key-value pairs in
+    [pairs] but with the same id as [document]. *)
 
 val update_data : (key * value) list -> t -> t
-(** update a document without overwriting the entire document *)
+(** [update_data pairs document] is the document after the each value in
+    [document] for some key in [pairs] has been replaced by the corresponding
+    value in [pairs]. Raises: [Not_found] If [pairs] contains some keys that
+    can't be found in [document],*)
 
 val delete_field : key -> t -> t
-(** delete a field in a document *)
+(** [delete_field key document] is the document after the key [key] has been
+    removed in [dcoument]. *)
 
 val document_id : t -> string
-(** return the id of a document *)
+(** [document_id document] is the id of [document]. *)
 
 val data : t -> (key * value) list
-(** return the data of a document *)
+(** [data document] is a list of key-value pairs stored in [document]. *)
 
 val string_of_document : t -> string
-(** return string representation of document*)
+(** [string_of_document document] is a string representation of [document]*)

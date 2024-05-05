cbb7d355f76df1bc59d4030149610333ecfadd62
Thomas Lu <cl2625@cornell.edu>
2024-05-05 18:26:21 -0400

    Refactored where_field -- supports comparison of all value types -- perform preemptive type checks to catch errors

--- lib/collection.ml
+++ lib/collection.ml
@@ -27,6 +27,18 @@ let set_document doc col =
   { name = col.name; documents = doc :: filtered_col }
 
 let where_field field query col =
+  let check_type query =
+    match query with
+    | Is_greater_than value
+    | Is_less_than value
+    | Is_greater_than_or_equal_to value
+    | Is_less_than_or_equal_to value -> (
+        match value with
+        | Map _ | List _ -> raise Value.Type_error
+        | _ -> ())
+    | _ -> ()
+  in
+  check_type query;
   let documents' =
     List.filter
       (fun doc -> List.assoc_opt field (data doc) <> None)
@@ -34,23 +46,33 @@ let where_field field query col =
   in
   let col = { col with documents = documents' } in
   let get_field field doc = List.assoc field (data doc) in
+  let equal_to value doc = Value.compare value (get_field field doc) = 0 in
+  let not_equal_to value doc = not (equal_to value doc) in
+  let is_in values doc = List.exists (fun value -> equal_to value doc) values in
+  let is_not_in values doc = not (is_in values doc) in
+  let greater_than value doc = Value.compare value (get_field field doc) = 1 in
+  let less_than value doc = Value.compare value (get_field field doc) = -1 in
+  let greater_than_or_equal_to value doc =
+    greater_than value doc || equal_to value doc
+  in
+  let less_than_or_equal_to value doc =
+    greater_than value doc || equal_to value doc
+  in
   {
     name = col.name;
     documents =
-      (match query with
-      | Is_equal_to value ->
-          List.filter (fun doc -> get_field field doc = value) col.documents
-      | Is_not_equal_to value ->
-          List.filter (fun doc -> get_field field doc <> value) col.documents
-      | Is_in values ->
-          List.filter
-            (fun doc -> List.mem (get_field field doc) values)
-            col.documents
-      | Is_not_in values ->
-          List.filter
-            (fun doc -> not (List.mem (get_field field doc) values))
-            col.documents
-      | _ -> raise (Invalid_argument "Query not supported"));
+      (let f =
+         match query with
+         | Is_equal_to value -> equal_to value
+         | Is_not_equal_to value -> not_equal_to value
+         | Is_in values -> is_in values
+         | Is_not_in values -> is_not_in values
+         | Is_greater_than value -> greater_than value
+         | Is_less_than value -> less_than value
+         | Is_greater_than_or_equal_to value -> greater_than_or_equal_to value
+         | Is_less_than_or_equal_to value -> less_than_or_equal_to value
+       in
+       List.filter f col.documents);
   }
 
 let delete document col =
@@ -82,7 +104,5 @@ let to_json col filename =
   close_out oc
 
 let union _ _ = failwith "not implemented"
-
-let intersect  _ _ = failwith "not implemented"
-
-let difference  _ _ = failwith "not implemented"
+let intersect _ _ = failwith "not implemented"
+let difference _ _ = failwith "not implemented"

33a22662fef8e2032524ba10a53cae086f8bf057
Daniel Wang <zw669@cornell.edu>
2024-05-08 02:52:14 -0400

    Add equals method
    - Added another method called equals

--- lib/document.ml
+++ lib/document.ml
@@ -88,3 +88,11 @@ let difference doc1 doc2 =
     List.filter (fun (k, _) -> not (List.mem_assoc k data2)) data1
   in
   set_data diff_data doc1
+
+let equals doc1 doc2 =
+  let data1 = data doc1 in
+  let data2 = data doc2 in
+  List.length data1 = List.length data2
+  && List.for_all
+       (fun (k, v) -> List.mem_assoc k data2 && v = List.assoc k data2)
+       data1

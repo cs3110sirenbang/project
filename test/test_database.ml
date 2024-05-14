b98efd54f6b9f63f8899126212048a8cdf91d35e
Steven Yu <ssy38@cornell.edu>
2024-05-14 18:18:06 -0400

    Fix test_database
    - removed file suffix check from read implementation as it is not necessary
    - fix errors for file deps
    
    Co-authored-by: Thomas Lu <ThomasLu2333@users.noreply.github.com>

--- test/test_database.ml
+++ test/test_database.ml
@@ -8,22 +8,14 @@ let test_make _ =
     (Database.get_name db);
   assert_bool "Last updated should be set" (Database.get_last_updated db > 0.0)
 
-(* let test_string_of_database _ = let db = Database.make "test_db" in let
-   output = Database.string_of_database db in let expected = "{\"name\":
-   \"test_db\"\n,\"last_updated\": " ^ string_of_float
-   (Database.get_last_updated db) ^ "\n\n\"collections\":
-   \n[]\n\n\"prev_collections\": \n[]\n}" in assert_equal ~msg:"Database
-   serialization is incorrect" expected output *)
-
 let test_read_write _ =
-  let filename = "./data/temp_db.cs" in
+  let filename = "./data/temp_db.json" in
   let db_read = read filename in
   assert_equal ~msg:"Read database name does not match" "SampleDB"
     (Database.get_name db_read);
   assert_equal ~msg:"Users collection should exist & have 2 documents" 2
     (List.length
-       (Collection.get_documents
-          (Database.get_collection (Database.get_name db_read) db_read)))
+       (Collection.get_documents (Database.get_collection "Users" db_read)))
 
 let test_set_delete_rollback _ =
   let db = Database.make "test_db" in
@@ -35,7 +27,7 @@ let test_set_delete_rollback _ =
   assert_raises Not_found (fun () ->
       Database.get_collection "test_collection" db);
   Database.rollback db;
-  assert_equal ~msg:"Rollback did not restore collection" col
+  assert_equal ~msg:"Rollback did not restore\n   collection" col
     (Database.get_collection "test_collection" db)
 
 let database_tests =
@@ -44,7 +36,7 @@ let database_tests =
          "test_make" >:: test_make;
          (* "test_string_of_database" >:: test_string_of_database; *)
          "test_read_write" >:: test_read_write;
-         (* "test_set_delete_rollback" >:: test_set_delete_rollback; *)
+         "test_set_delete_rollback" >:: test_set_delete_rollback;
        ]
 
 let () = run_test_tt_main database_tests

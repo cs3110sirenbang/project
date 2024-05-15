71246a54d33226bc3ade4994ee0953b0f7d6075b
Steven Yu <ssy38@cornell.edu>
2024-05-15 00:46:17 -0400

    Fix test_main formatting
    - fix ocamlformat of test_main

--- test/test_main.ml
+++ test/test_main.ml
@@ -6,4 +6,14 @@ open Test_database
 open OUnit2
 
 let run_suites lst = List.iter (fun suite -> run_test_tt_main suite) lst
-let _ = run_suites [ value_tests; collection_tests; document_tests; database_tests; parser_tests; lex_tests ]
+
+let _ =
+  run_suites
+    [
+      value_tests;
+      collection_tests;
+      document_tests;
+      database_tests;
+      parser_tests;
+      lex_tests;
+    ]
